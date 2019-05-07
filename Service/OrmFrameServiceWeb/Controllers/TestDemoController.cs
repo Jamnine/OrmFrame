723a5d7792b576f725c395096c91355587324a3d
Ninth-MrJ <cuz_mr.j@live.com>
2019-05-07 14:04:41 +0800

    service

--- Service/OrmFrameServiceWeb/Controllers/TestDemoController.cs
+++ Service/OrmFrameServiceWeb/Controllers/TestDemoController.cs
@@ -1,14 +1,40 @@
-﻿using System;
+﻿using Orm.Framework.Common;
+using Orm.Model.Parameter;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using Orm.Services.TestSercive;
 
 namespace OrmFrameServiceWeb.Controllers
 {
-    public class TestDemoController : ApiController
+    public class TestDemoController : BaseApiController
     {
-
+        TestDemoServices testDemoServices = new TestDemoServices();
+        [HttpGet, HttpPost]
+        public ReturnValue UpdateItemByCode()
+        {
+            try
+            {
+                string model = QueryStringHelper.GetString("model", "");
+                if (string.IsNullOrEmpty(model))
+                {
+                    returnValue.ErrorCode = 1;
+                    returnValue.ErrorMsg = "model不能为空";
+                    return returnValue;
+                }
+                string feemz = QueryStringHelper.GetString("feemz", "");
+                returnValue.ShowMsg = testDemoServices.GetLocHouseRoomName("feemz").GUID;
+                return returnValue;
+            }
+            catch (Exception ex)
+            {
+                returnValue.ErrorCode = 1;
+                returnValue.ErrorMsg = ex.Message;
+                return returnValue;
+            }
+        }
     }
 }
